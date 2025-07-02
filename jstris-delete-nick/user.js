caafeaa98f16e1da438135dc5fab8dc65ca65ec4
sasa24 <hoge@hoge>
2025-07-02 19:59:59 +0900

    [jstris-delete-nick] 新規追加

---
+++ jstris-delete-nick/user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         delete 'nick' from localStorage
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Delete the 'nick' key from localStorage when accessing the site
+// @author       You
+// @match        https://jstris.jezevec10.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jezevec10.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    localStorage.removeItem('nick');
+})();
